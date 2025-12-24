47da80de3c2675dd9269830c05f40dbd48cc96cd
henrique vigato <henriquevigato@gmail.com>
2025-12-24 17:46:14 -0300

    Fazendo o REPL no main.go

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+
+		success := scanner.Scan()
+		if !success {
+			fmt.Fprintln(os.Stderr, "Wasn't possible read a message")
+			break
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "shouldn't see an error scannig a string")
+		}
+
+		message := scanner.Text()
+		cleanedMessage := cleanInput(message)
+
+		if len(cleanedMessage) == 0 {
+			fmt.Println("Comando vazio")
+			continue
+		}
+
+		firstMessage := cleanedMessage[0]
+
+		fmt.Println("Your command was: ", firstMessage)
+	}
 }
