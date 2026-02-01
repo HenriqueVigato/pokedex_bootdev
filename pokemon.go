c0d7780065521caaf07d6f95526af83a7e8366b8
henrique vigato <henriquevigato@gmail.com>
2026-02-01 19:57:56 -0300

    Criando a funcao que transforma o json em um struct
    Criando a funcao de ajuda que cria a struct Pokemon e converto o json
    para a struct

---
+++ pokemon.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Pokemon struct {
+	Forms []struct {
+		Name string `json:"name"`
+	}
+	Heigth int `json:"height"`
+	Weight int `json:"weight"`
+	Stats  []struct {
+		BaseStat int `json:"base_stat"`
+		Stat     struct {
+			Name string `json:"name"`
+		} `json:"stat"`
+	} `json:"stats"`
+	Types []struct {
+		Type struct {
+			Name string `json:"name"`
+		} `json:"type"`
+	} `json:"types"`
+}
+
+func ConvertToStruct(pokeData []byte) (Pokemon, error) {
+	var pokemon Pokemon
+	if erro := json.Unmarshal(pokeData, &pokemon); erro != nil {
+		fmt.Println(erro)
+		return Pokemon{}, erro
+	}
+
+	return pokemon, nil
+}
